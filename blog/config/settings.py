90499942b475bdc4d9e619e91cd2bbac18b5a9fb
K1R1LL117 <n17k17@yandex.ru>
2025-04-06 15:35:39 +0300

    fix

--- blog/config/settings.py
+++ blog/config/settings.py
@@ -24,8 +24,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # Django libs
-    # Developers Apps
     "posts.apps.PostsConfig",
     "users.apps.UsersConfig",
 ]
